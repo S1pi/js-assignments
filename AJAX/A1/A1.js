c10873e6233112d8de5e04284bba26175ac5da8d
S1pi <miikavsipila@gmail.com>
2024-09-01 21:20:24 +0300

    AJAX Assignments

---
+++ AJAX/A1/A1.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const fetchUData = async () => {
+  const response = await fetch('https://reqres.in/api/users/1');
+  const data = await response.json();
+  console.log(data);
+};
+fetchUData();
